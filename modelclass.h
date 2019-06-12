6b6cd29a34a60a385b2b499cfa91f071010d6c6f
sportsradical <peereira@live.com.pt>
2019-06-12 22:04:35 +0100

    Add files via upload

---
+++ modelclass.h
@@ -0,0 +1,55 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: modelclass.h
+////////////////////////////////////////////////////////////////////////////////
+#ifndef _MODELCLASS_H_
+#define _MODELCLASS_H_
+
+
+//////////////
+// INCLUDES //
+//////////////
+#include <d3d11.h>
+#include <directxmath.h>
+using namespace DirectX;
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: ModelClass
+////////////////////////////////////////////////////////////////////////////////
+class ModelClass
+{
+private:
+	struct VertexType
+	{
+		XMFLOAT3 position;
+		XMFLOAT4 color;
+	};
+
+public:
+	ModelClass();
+	ModelClass(const ModelClass&);
+	~ModelClass();
+
+	bool Initialize(ID3D11Device*);
+	void Shutdown();
+	void Update();
+	void Render(ID3D11DeviceContext*);
+
+	int GetIndexCount();
+	void SetPosition(float positionX, float positionY, float positionZ);
+	XMFLOAT3 GetPosition();
+
+	
+	
+private:
+	bool InitializeBuffers(ID3D11Device*);
+	void ShutdownBuffers();
+	void RenderBuffers(ID3D11DeviceContext*);
+
+private:
+	ID3D11Buffer *m_vertexBuffer, *m_indexBuffer;
+	int m_vertexCount, m_indexCount;
+	XMFLOAT3 m_position;
+};
+
+#endif
